7956fa38ded6a63e4bd8e38f0840c00d2d1dcbab
100whitebirds <avenlardo@gmail.com>
2023-08-30 02:04:01 +0600

    PWA setup

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,8 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import WorkboxPlugin from 'workbox-webpack-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import StatoscopeWebpackPlugin from '@statoscope/webpack-plugin';
 import { BuildOptions } from './types/config';
@@ -12,6 +14,7 @@ export function buildPlugins({
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   const plugins = [
     new HtmlWebpackPlugin({
+      title: 'PaperBox WPA',
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
@@ -23,12 +26,32 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
     }),
+
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public/images', to: 'images',
+        },
+        {
+          from: 'public/manifest.json', to: 'manifest',
+        },
+      ],
+    }),
   ];
 
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin());
     plugins.push(new webpack.HotModuleReplacementPlugin());
     plugins.push(new StatoscopeWebpackPlugin({
+      // change to 'file' to open the report
+      // open: false,
       open: 'file',
     }));
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
+import { App } from 'app/App';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
-root.render(<div>TEST TEST</div>);
+root.render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      }).catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
